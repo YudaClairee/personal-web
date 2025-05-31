b0214fa7b34fdc70241b39562bf05a01ae151f96
Djibrani Yuda <djibyuda@gmail.com>
2025-05-31 18:18:32 +0700

    init repo

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Typed from "typed.js";
+
+const myTyped = new Typed("#role", {
+  strings: [
+    "Aspiring Frontend Developer",
+    "Long Life Learner",
+    "Blockchain Enthusiast",
+  ],
+  loop: true,
+  typeSpeed: 50,
+  backSpeed: 50,
+});
